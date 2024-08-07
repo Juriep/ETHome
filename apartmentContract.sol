a1da9a9b7d19e0c1805e13b4540eceb247db17ca
Juriep <sebas130399@gmail.com>
2024-08-06 23:58:50 -0500

    apartment contracth enhanced, new methods implemented

--- apartmentContract.sol
+++ apartmentContract.sol
@@ -10,76 +10,81 @@ contract apartmentContract {
     // store apartments and find them by their city direction
     mapping (string => Apartment) private apartments;
 
-    event NewApartmentAdded(string _apartmentDirection, string _description, uint _ethPrice, ApartmentState _aptState ,address _apartmentOwner);
-    event ApartmentDeleted(string _apartmentDirection);
-    event ApartmentUpdated(string _apartmentDirection, string _description, uint _ethPrice, ApartmentState _aptState ,address _apartmentOwner);
-    event ApartmentDirectionChanged(string _oldDirection, string _newDirection);
+    event NewApartmentAdded(string _apartmentAddress, string _description, uint _price, ApartmentState _aptState ,address _apartmentOwner);
+    event ApartmentDeleted(string _apartmentAddress);
+    event ApartmentUpdated(string _apartmentAddress, string _description, uint _price, ApartmentState _aptState ,address _apartmentOwner);
+    event ApartmentDirectionChanged(string _oldApartmentAddress, string _newApartmentAdress);
 
     struct Apartment{
-        string apartmentDirection;
+        string apartmentAddress;
         string description;
-        uint ethPrice;
+        uint price;
         ApartmentState aptState;
         address apartmentOwner;
     }
 
-    modifier onlyNewApartment(string memory _apartmentDirection){
+    modifier onlyNewApartment(string memory _apartmentAddress){
 
-        require( bytes(apartments[_apartmentDirection].apartmentDirection).length == 0, "Apartment already exists!");
+        require( bytes(apartments[_apartmentAddress].apartmentAddress).length == 0, "Apartment already exists!");
         _;
 
     }
 
-    modifier ifApartmentExists(string memory _apartmentDirection) {
-        require( bytes(apartments[_apartmentDirection].apartmentDirection).length != 0, "Apartment does not exist!");
+    modifier ifApartmentExists(string memory _apartmentAddress) {
+        require( bytes(apartments[_apartmentAddress].apartmentAddress).length != 0, "Apartment does not exist!");
         _;
     }
 
-    modifier ifOldApartmentExist(string memory _apartmentDirection){
-        require( bytes(apartments[_apartmentDirection].apartmentDirection).length != 0, "Apartment does not exist!");
+    modifier ifOldApartmentExist(string memory _apartmentAddress){
+        require( bytes(apartments[_apartmentAddress].apartmentAddress).length != 0, "Apartment does not exist!");
         _;
     }
 
 
-    function addApartment(string memory _apartmentDirection, string memory _description, uint _ethPrice, ApartmentState _aptState ,address _apartmentOwner) public 
-        onlyNewApartment(_apartmentDirection)
+    function addApartment(string memory _apartmentAddress, string memory _description, uint _price, ApartmentState _aptState ,address _apartmentOwner) public 
+        onlyNewApartment(_apartmentAddress)
     {
-        apartments[_apartmentDirection] = Apartment(_apartmentDirection,  _description, _ethPrice, _aptState, _apartmentOwner);
-        emit NewApartmentAdded(_apartmentDirection,  _description, _ethPrice, _aptState, _apartmentOwner);
+        apartments[_apartmentAddress] = Apartment(_apartmentAddress,  _description, _price, _aptState, _apartmentOwner);
+        emit NewApartmentAdded(_apartmentAddress,  _description, _price, _aptState, _apartmentOwner);
     }
 
-    function getApartment(string memory _apartmentDirection) public view ifApartmentExists(_apartmentDirection) returns (Apartment memory)
+    function getApartment(string memory _apartmentAddress) public view ifApartmentExists(_apartmentAddress) returns (Apartment memory)
     {
-        return apartments[_apartmentDirection];
+        return apartments[_apartmentAddress];
     }
 
-    function updateApartmentParams(string memory _apartmentDirection, string memory _description, uint _ethPrice, ApartmentState _aptState,address _apartmentOwner) public ifApartmentExists(_apartmentDirection)
+    function getApartmentState(string memory _apartmentAddress) public view returns (ApartmentState)
     {
-        apartments[_apartmentDirection].description = _description;
-        apartments[_apartmentDirection].ethPrice = _ethPrice;
-        apartments[_apartmentDirection].aptState = _aptState;
-        apartments[_apartmentDirection].apartmentOwner = _apartmentOwner;
-
-        emit ApartmentUpdated(_apartmentDirection,  _description, _ethPrice, _aptState, _apartmentOwner);
+        return apartments[_apartmentAddress].aptState;
     }
 
-    function updateApartmentDirection(string memory _oldDirection, string memory _newDirection) public ifOldApartmentExist(_oldDirection){
+    function updateApartmentParams(string memory _apartmentAddress, string memory _description, uint _price, ApartmentState _aptState,address _apartmentOwner) public ifApartmentExists(_apartmentAddress)
+    {
+        apartments[_apartmentAddress].description = _description;
+        apartments[_apartmentAddress].price = _price;
+        apartments[_apartmentAddress].aptState = _aptState;
+        apartments[_apartmentAddress].apartmentOwner = _apartmentOwner;
+
+        emit ApartmentUpdated(_apartmentAddress,  _description, _price, _aptState, _apartmentOwner);
+    }
+
+    function updateApartmentDirection(string memory _oldApartmentAddress, string memory _newApartmentAdress) public ifOldApartmentExist(_oldApartmentAddress){
         
-        apartments[_newDirection].apartmentDirection = _newDirection;
-        apartments[_newDirection].description = apartments[_oldDirection].description;
-        apartments[_newDirection].ethPrice = apartments[_oldDirection].ethPrice;
-        apartments[_newDirection].aptState = apartments[_oldDirection].aptState;
-        apartments[_newDirection].apartmentOwner = apartments[_oldDirection].apartmentOwner;
+        apartments[_newApartmentAdress].apartmentAddress = _newApartmentAdress;
+        apartments[_newApartmentAdress].description = apartments[_oldApartmentAddress].description;
+        apartments[_newApartmentAdress].price = apartments[_oldApartmentAddress].price;
+        apartments[_newApartmentAdress].aptState = apartments[_oldApartmentAddress].aptState;
+        apartments[_newApartmentAdress].apartmentOwner = apartments[_oldApartmentAddress].apartmentOwner;
 
-        delete apartments[_oldDirection];
+        delete apartments[_oldApartmentAddress];
 
-        emit ApartmentDirectionChanged(_oldDirection, _newDirection);
+        emit ApartmentDirectionChanged(_oldApartmentAddress, _newApartmentAdress);
     }
     
-    function deleteApartment(string memory _apartmentDirection) ifApartmentExists(_apartmentDirection)
+    function deleteApartment(string memory _apartmentAddress) public ifApartmentExists(_apartmentAddress)
     {
-        delete apartments[_apartmentDirection];
-        emit ApartmentDeleted(_apartmentDirection);
+        delete apartments[_apartmentAddress];
+        emit ApartmentDeleted(_apartmentAddress);
     }
 
 }
